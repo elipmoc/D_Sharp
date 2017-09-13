ef86b183e678cea9d4f68de217f7b4d97a802f12
パン粉 ライ麦 <relipmoc101@gmail.com>
2017-09-13 19:51:05 +0900

    四則演算と()に対応した

--- D_Sharp/CreateTree.cs
+++ D_Sharp/CreateTree.cs
@@ -10,6 +10,7 @@ namespace D_Sharp
     static class CreateTree
     {
 
+        //二項演算子の生成
         static Expression CreateBinaryOperator(Expression left,Expression right,string str)
         {
             switch (str)
@@ -26,6 +27,7 @@ namespace D_Sharp
             return null;
         }
 
+        //文
         static public Func<double> CreateStatement(TokenStream tokenst)
         {
             var body = CreateSiki(tokenst);
@@ -34,49 +36,84 @@ namespace D_Sharp
             return Expression.Lambda<Func<double>>(body).Compile();
         }
 
+        //式
         static Expression CreateSiki(TokenStream tokenst)
         {
             tokenst.SetCheckPoint();
             Expression left;
-            if ((left=CreateInsi(tokenst)) != null)
+            if ((left = CreateKou(tokenst)) != null)
             {
-                string op = tokenst.Get().Str;
-                if (op== "+" ||op=="-")
+                Expression right;
+                string op;
+                while (tokenst.NowIndex < tokenst.Size && (tokenst.Get().Str == "+" || tokenst.Get().Str == "-"))
                 {
-                    Expression right;
+                    op = tokenst.Get().Str;
                     tokenst.Next();
-                    if ((right = CreateInsi(tokenst)) != null)
+                    if ((right = CreateKou(tokenst)) == null)
                     {
-                        Expression body =
-                            CreateBinaryOperator(left,right,op);
-                        while (tokenst.NowIndex<tokenst.Size && (tokenst.Get().Str=="+"||tokenst.Get().Str=="-"))
-                        {
-                            op = tokenst.Get().Str;
-                            tokenst.Next();
-                            if ((right = CreateInsi(tokenst)) == null)
-                            {
-                                tokenst.Rollback();
-                                return null;
-                            }
-                            body =
-                                 CreateBinaryOperator(body,right,op);
-                        }
-                        return body;
+                        tokenst.Rollback();
+                        return null;
                     }
+                    left =
+                         CreateBinaryOperator(left, right, op);
                 }
+                return left;
             }
             tokenst.Rollback();
             return null;
         }
 
+        //項
+        static Expression CreateKou(TokenStream tokenst)
+        {
+            tokenst.SetCheckPoint();
+            Expression left;
+            if ((left = CreateInsi(tokenst)) != null)
+            {
+                Expression right;
+                string op;
+                while (tokenst.NowIndex < tokenst.Size && (tokenst.Get().Str == "*" || tokenst.Get().Str == "/"))
+                {
+                    op = tokenst.Get().Str;
+                    tokenst.Next();
+                    if ((right = CreateInsi(tokenst)) == null)
+                    {
+                        tokenst.Rollback();
+                        return null;
+                    }
+                    left =
+                         CreateBinaryOperator(left, right, op);
+                }
+                return left;
+            }
+            tokenst.Rollback();
+            return null;
+        }
+
+        //因子
         static Expression CreateInsi(TokenStream tokenst)
         {
             tokenst.SetCheckPoint();
+            //実数
             if (tokenst.Get().TokenType == TokenType.Double)
             {
                 var constant_double = Expression.Constant(tokenst.Get().GetDouble());
                 tokenst.Next();
                 return constant_double;
+            }
+            // ( 式 ) 
+            else if (tokenst.Get().Str == "(")
+            {
+                Expression expr;
+                tokenst.Next();
+                if ((expr = CreateSiki(tokenst)) != null)
+                {
+                    if (tokenst.Get().Str == ")")
+                    {
+                        tokenst.Next();
+                        return expr;
+                    }
+                }
             }
             tokenst.Rollback();
             return null;
